04d8baa3aba0ff4585b63098f9d57fea0985bb37
huyhoangttql <huyhoangttql@gmail.com>
2021-12-16 10:53:10 +0700

    Update

---
+++ lib/models/posts.dart
@@ -0,0 +1,7 @@
+class Posts {
+  final String username;
+  final String described;
+  final String timeAgo;
+  Posts(
+      {required this.username, required this.described, required this.timeAgo});
+}
